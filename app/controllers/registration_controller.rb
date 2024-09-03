30ec0c602e211185264ef3a2b7e1c91785acf15d
netdy <netdy@odds.team>
2024-09-03 13:46:00 +0700

    feat : save data into database

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,5 +3,19 @@ class RegistrationController < ApplicationController
   end
 
   def create
+    puts "--------------------------------------------------------------"
+    @user_info = UserInfo.new(user_info_params)
+    if @user_info.save
+      puts "--------------------------------------------------------------"
+      puts "User info saved"
+    else
+      puts "--------------------------------------------------------------"
+      puts "User info not saved"
+    end
+  end
+
+  private
+  def user_info_params
+    params.require(:user_info).permit(:FirstName, :LastName, :DateOfBirth, :Gender, :Email, :PhoneNumber, :Subject)
   end
 end
