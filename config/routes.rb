0551c99e7c3aa64a0acd567127191e5adf4b73ad
netdy <netdy@odds.team>
2024-09-03 13:12:31 +0700

    feat : change routes path and create for post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "registration/index"
+  get 'registration', to: 'registration#index'
+  post 'registration', to: 'registration#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
