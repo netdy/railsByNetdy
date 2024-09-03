54228d377b0392e89117c5f911fc3dba53073ded
netdy <netdy@odds.team>
2024-09-03 19:29:47 +0700

    feat : If all fields are not filled in cannot submit

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,2 +1,9 @@
 class UserInfo < ApplicationRecord
+  validates :FirstName, presence: true
+  validates :LastName, presence: true
+  validates :DateOfBirth, presence: true
+  validates :Gender, presence: true
+  validates :Email, presence: true
+  validates :PhoneNumber, presence: true
+  validates :Subject, presence: true
 end
