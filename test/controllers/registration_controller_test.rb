c36fdd1d2ff180d5d2b62b875862fdf24c43fc17
netdy <netdy@odds.team>
2024-09-04 15:56:52 +0700

    test : add test for model UseInfo

--- test/controllers/registration_controller_test.rb
+++ test/controllers/registration_controller_test.rb
@@ -1,8 +1,35 @@
 require "test_helper"
 
 class RegistrationControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @valid_user_info_params = {
+      user_info: {
+        FirstName: "John",
+        LastName: "Doe",
+        DateOfBirth: "2000-01-01",
+        Gender: "Male",
+        Email: "john.doe@example.com",
+        PhoneNumber: "1234567890",
+        Subject: "Front-end"
+      }
+    }
+
+    @invalid_user_info_params = {
+      user_info: {
+        FirstName: "",
+        LastName: "",
+        DateOfBirth: "",
+        Gender: "Unknown",
+        Email: "invalid_email",
+        PhoneNumber: "12345",
+        Subject: ""
+      }
+    }
+  end
+
   test "should get index" do
-    get registration_index_url
+    get registration_url
     assert_response :success
+    assert_select 'form'
   end
 end
--- test/models/user_info_test.rb
+++ test/models/user_info_test.rb
@@ -1,7 +1,75 @@
 require "test_helper"
 
 class UserInfoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user_info = UserInfo.new(
+      FirstName: "Netdy",
+      LastName: "Naja",
+      DateOfBirth: "2002-05-27",
+      Gender: "Female",
+      Email: "john.doe@example.com",
+      PhoneNumber: "0951456789",
+      Subject: "Front-end"
+    )
+  end
+
+  test "should be valid with valid attributes" do
+    assert @user_info.valid?
+  end
+
+  test "should not be valid without a first name" do
+    @user_info.FirstName = nil
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid without a last name" do
+    @user_info.LastName = nil
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid without a date of birth" do
+    @user_info.DateOfBirth = nil
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid without a gender" do
+    @user_info.Gender = nil
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid with an invalid gender" do
+    @user_info.Gender = "Non-binary"
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid without an email" do
+    @user_info.Email = nil
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid with a duplicate email" do
+    duplicate_user_info = @user_info.dup
+    @user_info.save
+    assert_not duplicate_user_info.valid?
+  end
+
+  test "should not be valid with an invalid email format" do
+    @user_info.Email = "invalid_email"
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid without a phone number" do
+    @user_info.PhoneNumber = nil
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid with an invalid phone number format" do
+    @user_info.PhoneNumber = "12345"
+    assert_not @user_info.valid?
+  end
+
+  test "should not be valid without a subject" do
+    @user_info.Subject = nil
+    assert_not @user_info.valid?
+  end
 end
