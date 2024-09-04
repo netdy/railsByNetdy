6f631bdb02960558418fd9ca69b96f10078790fc
netdy <netdy@odds.team>
2024-09-05 00:43:24 +0700

    fix : rubocop -a for lint

--- test/controllers/setpassword_controller_test.rb
+++ test/controllers/setpassword_controller_test.rb
@@ -1,26 +1,26 @@
-require 'test_helper'
-require 'mocha/minitest'
+require "test_helper"
+require "mocha/minitest"
 
 class SetpasswordControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = UserInfo.create!(
-      Email: 'test@example.com',
-      FirstName: 'John',
-      LastName: 'Doe',
+      Email: "test@example.com",
+      FirstName: "John",
+      LastName: "Doe",
       DateOfBirth: Date.new(1990, 1, 1),
-      Gender: 'Male',
-      PhoneNumber: '1234567890',
-      Subject: 'Math'
+      Gender: "Male",
+      PhoneNumber: "1234567890",
+      Subject: "Math"
     )
   end
 
   test "should not update password when passwords don't match" do
     mock_session_for(@user)
     post setpassword_url, params: {
-      setpassword: { password: 'newpassword', password_confirm: 'differentpassword' }
+      setpassword: { password: "newpassword", password_confirm: "differentpassword" }
     }
     assert_response :success
-    assert_match 'Passwords do not match', response.body
+    assert_match "Passwords do not match", response.body
   end
 
 
