3a14ebc61c501d303b0a56da600e23f362e335d2
netdy <netdy@odds.team>
2024-09-04 21:25:44 +0700

    feat : check same password and save password in database

--- app/controllers/setpassword_controller.rb
+++ app/controllers/setpassword_controller.rb
@@ -3,6 +3,23 @@ class SetpasswordController < ApplicationController
   end
 
   def create
-    
+    email = session[:user_email]
+    password= params[:setpassword][:password]
+    password_confirmation = params[:setpassword][:password_confirm]
+    puts "--------------------------------------------------------------"
+    puts "Email: #{email}"
+    puts "Password: #{password}"
+    puts "Password Confirmation: #{password_confirmation}"
+    if password != password_confirmation
+      puts "--------------------------------------------------------------"
+      puts "Passwords do not match"
+      render turbo_stream: turbo_stream.update("error-messages", partial: "setpassword/form_errors", locals: { errors: [ "Passwords do not match" ] })
+    else
+      @user = UserInfo.find_by(Email: email)
+      @user.update(password: password)
+      puts "--------------------------------------------------------------"
+      puts "Password updated"
+      redirect_to controller: :page, action: :index
+    end
   end
 end
