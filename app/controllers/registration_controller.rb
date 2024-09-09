b1ff86a88560bde911f6c27314a45b7782850e43
netdy <netdy@odds.team>
2024-09-09 21:11:55 +0700

    EXAM : change date_feild to select , change show year in Buddhist era

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -6,6 +6,8 @@ class RegistrationController < ApplicationController
   def create
     # puts "--------------------------------------------------------------"
     @user_info = UserInfo.new(user_info_params)
+    @user_info.DateOfBirth = Date.parse(params[:user_info][:DateOfBirth]) if params[:user_info][:DateOfBirth].present?
+
     if @user_info.save
       flash[:success] = "registration success"
       redirect_to action: :index
