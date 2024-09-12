058cd1a6e6ed64f535cdf5804245570bc8a96ca1
netdy <netdy@odds.team>
2024-09-12 16:57:52 +0700

    fix rubocop -a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def manifest
-    render json: File.read(Rails.root.join('public', 'manifest.json')), content_type: 'application/json'
+    render json: File.read(Rails.root.join("public", "manifest.json")), content_type: "application/json"
   end
 end
