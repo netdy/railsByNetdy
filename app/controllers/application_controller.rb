04903f8be180a83924258ed1659e9314eba39956
netdy <netdy@odds.team>
2024-09-13 09:44:44 +0700

    fix date and pwa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  #allow_browser versions: :modern
   def manifest
     render json: File.read(Rails.root.join("public", "manifest.json")), content_type: "application/json"
   end
---
+++ app/javascript/controllers/date_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["dateField"];
+
+  connect() {
+    this.setDefaultDate();
+  }
+
+  setDefaultDate() {
+    if (!this.dateFieldTarget.value) {
+      const currentYear = new Date().getFullYear();
+      const buddhistYear = currentYear + 543;
+
+      const formattedDate = this.formatDate(buddhistYear);
+      this.dateFieldTarget.value = formattedDate;
+    }
+  }
+
+  formatDate(buddhistYear) {
+    const date = new Date(this.dateFieldTarget.value || new Date()); 
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${buddhistYear}-${month}-${day}`;
+  }
+}
